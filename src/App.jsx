637e41a99903d1e32a90af51e67809f18760cedc
Thiago kauai <html5ephp@gmail.com>
2023-04-16 23:35:56 -0300

    adicionando modal map local

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { FiXCircle } from "react-icons/fi";
 import { FcGoogle } from 'react-icons/fc';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { getDatabase, ref, child, get,onValue,set } from "firebase/database";
@@ -16,6 +17,7 @@ const googleProvider = new GoogleAuthProvider();
 
 function App() {
   const [data, setData] = useState([]);
+  const [modal, setModal] = useState(false);
   useEffect(() => {
     onValue(starCountRef, (snapshot) => {
       const data = snapshot.val();
@@ -56,6 +58,11 @@ function App() {
   return (
     <div className="container">
       <div className="App">
+
+        {modal && <div className="modal" style={{display:modal ? 'block': 'none'}}>
+           <span onClick={() => setModal(!modal)}><FiXCircle className='text-2xl'/></span>
+           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3654.366410445583!2d-46.6540934!3d-23.6628512!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5ab65bbb9f19%3A0x54b8f1f86fddd3e3!2sPar%C3%B3quia%20Nossa%20Senhora%20Ref%C3%BAgio%20dos%20Pecadores!5e0!3m2!1sen!2sbr!4v1681697837411!5m2!1sen!2sbr" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>}
         <header>
           <div className="container-img">
             <img src="Lala.png" alt=""/>
@@ -65,7 +72,7 @@ function App() {
         </header>
         <section>
           {/* <Calendar onChange={onChange} value={value}  minDetail="year"/> */}
-          <button className="content-link">
+          <button onClick={() => setModal(!modal)} className="content-link">
             Local
           </button>
           <button className="content-link">
